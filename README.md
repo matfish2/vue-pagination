0ac69174af8d625b587eff0205b38e18d7b1fd4e
Matanya Fishaimer <matfish2@gmail.com>
2016-04-03 19:04:19 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Vue Pagination
+
+Simple, generic and non-intrusive pagination component for Vue.js.
+Presentation is based on bootstrap.
+
+- [Dependencies](#dependencies)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Options](#options)
+
+# Dependencies
+
+* Vue.js (>=1.0). Required.
+* Bootstrap (CSS). Optional.
+
+# Installation
+
+## Option 1
+
+Compile the code using `browserify` with the `stringify` transform
+
+    npm install vue-pagination
+
+Require the script:
+
+    var VuePagination = require('vue-pagination');
+
+## Option 2
+
+Import the [compiled standalone file](https://raw.githubusercontent.com/matfish2/vue-pagination/master/dist/vue-pagination.min.js) into your HTML, which will expose a global `VueTables` variable.
+
+# Usage
+
+## Register the component(s)
+
+    Vue.use(VuePagination);
+
+HTML:
+
+  <pagination for="some-entity" :records="500"></pagination>
+
+props:
+
+* `for` `string` `required` unique identifier for the component instance.
+* `records` `number` `required` number of records
+* `per-page` `number` `optional` records per page. Default: 25
+* `chunk` `number` `optional` max pages per chunk. Default: 10
+* `count-text` `string` `optional` total records text. Default: '{count} records'
+
+When a page is selected an event will be dispatched, using the unique id for the component.
+Listen to it and respond accordingly:
+
+this.$on('vue-pagination::some-entity', function(page) {
+    // display the relevant records using the page param
+});
+
---
+++ index.js
@@ -0,0 +1,82 @@
+exports.install = function(Vue) {
+
+ Vue.component('pagination', {
+  template: require('./src/template.html'),
+  data: function() {
+    return {
+      page: 1
+    }
+  },
+  props: {
+    for: {
+      type: String,
+      required: true
+    },
+    records: {
+      required: true
+    },
+    perPage: {
+      required: false,
+      default: 25
+    },
+    chunk: {
+      required: false,
+      default: 10
+    },
+    countText: {
+      type: String,
+      required: false,
+      default: '{count} records'
+    }
+  },
+  computed: {
+    pages: function() {
+      return range(1,this.chunk);
+    },
+    totalPages: function() {
+      return Math.ceil(this.records / this.perPage);
+    },
+    totalChunks: function() {
+      return Math.ceil(this.totalPages / this.chunk);
+    },
+    currentChunk: function() {
+      return Math.ceil(this.page / this.chunk);
+    },
+    paginationStart: function() {
+     return ((this.currentChunk-1) * this.chunk) + 1;
+    },
+    count: function() {
+      return this.countText.replace('{count}', this.records);
+    }
+  },
+  methods: {
+    setPage: function(page) {
+        this.page = page;
+        this.$dispatch('vue-pagination::' + this.for, page);
+    },
+    setChunk: function(direction) {
+        this.setPage((((this.currentChunk -1) + direction) * this.chunk) + 1);
+    },
+    allowedPage: function(direction) {
+      return (direction==1 && this.page<this.totalPages)
+      ||  (direction==-1 && this.page>1);
+    },
+    allowedChunk: function(direction) {
+      return (direction==1 && this.currentChunk<this.totalChunks)
+      ||  (direction==-1 && this.currentChunk>1);
+    },
+    isActive: function(index) {
+      return this.page==(index+1);
+    }
+  }
+});
+
+}
+
+function range(start, count) {
+  return Array.apply(0, Array(count))
+  .map(function (element, index) {
+   return index + start;
+ });
+}
+
